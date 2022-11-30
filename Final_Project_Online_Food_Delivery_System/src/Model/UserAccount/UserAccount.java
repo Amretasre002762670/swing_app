df6825b005f371cd9667aee83f5c95e27ab24d91
NagaVaishnavi-Puppala <puppala.n@northeastern.edu>
2022-11-30 18:53:49 -0500

    Created Model and UI packages and user account class

---
+++ Final_Project_Online_Food_Delivery_System/src/Model/UserAccount/UserAccount.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.UserAccount;
+
+/**
+ *
+ * @author puppalanagavaishnavi
+ */
+public class UserAccount {
+    
+     private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
