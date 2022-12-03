3869ab9af19e71ecf507050355110cf8d749328f
NagaVaishnavi-Puppala <puppala.n@northeastern.edu>
2022-12-02 23:26:17 -0500

    created ecosystem

---
+++ Final_Project_Online_Food_Delivery_System/src/Model/System/ConfigureaSystem.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.System;
+
+import Model.Employee.Employee;
+import Model.Roles.SystemAdminrole;
+import Model.UserAccount.UserAccount;
+
+/**
+ *
+ * @author puppalanagavaishnavi
+ */
+public class ConfigureaSystem {
+    public static Ecosystem configure(){
+        
+        Ecosystem system = Ecosystem.getInstance();
+        
+      
+        
+        
+        Employee employee = system.getEmployeeDirectory().createEmployee("RA");
+        
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SystemAdminrole());
+        
+        return system;
+}
+}
---
+++ Final_Project_Online_Food_Delivery_System/src/Model/System/Ecosystem.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.System;
+
+import Model.Customer.CustomerDirectory;
+import Model.DeliveryMan.DeliveryManDirectory;
+import Model.Restaurant.RestaurantDirectory;
+import Model.Roles.Role;
+import Model.Roles.SystemAdminrole;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author puppalanagavaishnavi
+ */
+public class Ecosystem extends Organization{
+    
+    private static Ecosystem business;
+    private CustomerDirectory customerDirectory;
+    private DeliveryManDirectory deliveryManDirectory;
+    private RestaurantDirectory restaurantDirectory;
+    
+    public static Ecosystem getInstance(){
+        if(business==null){
+            business=new Ecosystem();  
+        }
+        return business;
+    }
+
+    public static void setBusiness(Ecosystem business) {
+        Ecosystem.business = business;
+    }
+    private Ecosystem() {
+        super(null);
+        customerDirectory  = new CustomerDirectory();
+        deliveryManDirectory = new DeliveryManDirectory();
+        restaurantDirectory = new RestaurantDirectory();
+    }
+
+    public CustomerDirectory getCustomerDirectory() {
+        return customerDirectory;
+    }
+
+    public void setCustomerDirectory(CustomerDirectory customerDirectory) {
+        this.customerDirectory = customerDirectory;
+    }
+    
+    public DeliveryManDirectory getDeliveryManDirectory() {
+        return deliveryManDirectory;
+    }
+
+    public void setDeliveryManDirectory(DeliveryManDirectory deliveryManDirectory) {
+        this.deliveryManDirectory = deliveryManDirectory;
+    }
+
+    public RestaurantDirectory getRestaurantDirectory() {
+        return restaurantDirectory;
+    }
+
+    public void setRestaurantDirectory(RestaurantDirectory restaurantDirectory) {
+        this.restaurantDirectory = restaurantDirectory;
+    }
+    
+    
+   
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roleList=new ArrayList<Role>();
+        roleList.add(new SystemAdminrole());
+        return roleList;
+    }
+
+   
+}
+
+
