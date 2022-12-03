f2181328f9e9a5f1052baf892ede572a21d3d455
NagaVaishnavi-Puppala <puppala.n@northeastern.edu>
2022-12-03 15:12:21 -0500

    restaurant additions

--- Final_Project_Online_Food_Delivery_System/src/Model/Restaurant/RestaurantDirectory.java
+++ Final_Project_Online_Food_Delivery_System/src/Model/Restaurant/RestaurantDirectory.java
@@ -4,6 +4,8 @@
  */
 package Model.Restaurant;
 
+import Model.UserAccount.UserAccount;
+import Model.Customer.Customer;
 import java.util.ArrayList;
 
 /**
@@ -15,5 +17,45 @@ public class RestaurantDirectory {
 
     public RestaurantDirectory() {
         restaurantList = new ArrayList<>();
+    }
+    
+      public ArrayList<Restaurant> getRestaurantList() {
+        return restaurantList;
+    }
+
+    public void setRestaurentList(ArrayList<Restaurant> restaurantList) {
+        this.restaurantList = restaurantList;
+    }
+    
+ 
+    
+    public void deleteRestaurant(Restaurant restaurant) {
+        restaurantList.remove(restaurant);
+    }
+
+    
+ public Restaurant createRestaurant(){
+        Restaurant r = new Restaurant();
+        restaurantList.add(r);  
+        return r;
+    }
+  public boolean findRestaurant(String RestaurantName, String RestaurantPassword) {
+        boolean ifRestaurant = false;
+        for (Restaurant res: restaurantList) {
+            if (res.getRestaurantName().equals(RestaurantName) && res.getUserAccount().getPassword().equals(RestaurantPassword)) {
+                ifRestaurant = true;
+            }
+        }
+        return ifRestaurant;
+    }
+    
+    public Restaurant searchRestaurantProfile(String restaurantName) {
+        Restaurant resultRestaurant = null;
+        for (Restaurant res: restaurantList) {
+            if (res.getRestaurantName().equals(restaurantName)) {
+                  resultRestaurant = res;
+            }
+        }
+        return resultRestaurant;
     }
 }
